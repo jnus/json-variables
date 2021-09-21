f90b2ea0ef0fd15eef80ee7ff014fc20e5950728
Jasper Nygaard <jmn@seges.dk>
2021-09-22 00:16:20 +0200

    added linux friendly syntax

--- test/test.ps1
+++ test/test.ps1
@@ -5,13 +5,18 @@ Describe "Set-JsonVariables" {
         $configFile = "variables.minimal.json"
         
         It "Sets 2 env. variables for Dev" {
-            $result = & $SetJsonVariables Dev $configFile 
+            $result = . $SetJsonVariables Dev $configFile 
             $result.Count | Should -Be 2
         }
 
         It "Sets Url specific to Dev environment" {
-            $result = & $SetJsonVariables Dev $configFile 
+            $result = . $SetJsonVariables Dev $configFile 
             $result | Where-Object { $_ -like "*someDevHostName*"} | Should -BeTrue 
+        }
+
+        It "Does not set Url to DevTest environment" {
+            $result = . $SetJsonVariables Dev $configFile 
+            $result | Where-Object { $_ -like "*someDevTestHostName*"} | Should -BeFalse 
         }
     }
 }
