8cffb95600a33e80079324d8c44772b0ff0850ed
Jasper Nygaard <jmn@seges.dk>
2021-09-22 15:14:28 +0200

    refactored logic into module

--- test/test.ps1
+++ test/test.ps1
@@ -1,23 +1,23 @@
-$SetJsonVariables = "./../src/Set-JsonVariables.ps1"
+Import-Module  "./../src/JsonVariables.psm1" -Force
 Describe "Set-JsonVariables" {
     Context "Given config file is valid" {
 
         $configFile = "variables.minimal.json"
         
         It "sets 2 env. variables for Dev" {
-            $result = . $SetJsonVariables Dev $configFile 
+            $result = .  Set-JsonVariables Dev $configFile 
             
             $result.Count | Should -Be 2
         }
 
         It "sets Url specific to Dev environment" {
-            $result = . $SetJsonVariables Dev $configFile 
+            $result = Set-JsonVariables Dev $configFile 
             
             $result | Where-Object { $_ -like "*someDevHostName*"} | Should -BeTrue 
         }
 
         It "does not set Url to DevTest environment" {
-            $result = ./../src/set-jsonvariables.ps1 Dev $configFile 
+            $result =  Set-JsonVariables Dev $configFile 
             
             $result | Where-Object { $_ -like "*someDevTestHostName*"} | Should -BeFalse 
         }
@@ -28,7 +28,7 @@ Describe "Set-JsonVariables" {
         It "can parse a minimal configuration file" {
             $configFile = "variables.minimal.json"
             
-            $result = ./../src/set-jsonvariables.ps1 Dev $configFile
+            $result =  Set-JsonVariables Dev $configFile
             
             $result.Count | Should -Be 2
         }
@@ -36,7 +36,7 @@ Describe "Set-JsonVariables" {
         It "can parse a full configuration file" {
             $configFile = "variables.full.json"
             
-            $result = ./../src/set-jsonvariables.ps1 Dev $configFile
+            $result =  Set-JsonVariables Dev $configFile
             
             $result.Count | Should -Be 2
         }
@@ -44,7 +44,7 @@ Describe "Set-JsonVariables" {
         It "can parse a configuration file with normalized environments" {
             $configFile = "variables.full.json"
             
-            $result = ./../src/set-jsonvariables.ps1 Dev $configFile
+            $result =  Set-JsonVariables Dev $configFile
             
             $result.Count | Should -Be 2
         }
