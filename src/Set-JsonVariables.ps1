6f7fab01320c9a4828a040d2595078304cd651fe
Jasper Nygaard <jmn@seges.dk>
2021-10-05 20:50:22 +0200

    added correct paramter in test workflow

--- src/Set-JsonVariables.ps1
+++ src/Set-JsonVariables.ps1
@@ -9,6 +9,6 @@ param (
 )
 $here = Split-Path $MyInvocation.MyCommand.Definition
 
-Import-Module "$here\JsonVariables.psm1" -Force
+Import-Module "$here/JsonVariables.psm1" -Force
 
 Set-JsonVariables -TargetEnvironment $TargetEnvironment -ConfigFile $ConfigFile
