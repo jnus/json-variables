67d0ea8ba9d0426e2d9e91ae1f3203b4cf422503
Jasper Nygaard <jmn@seges.dk>
2021-10-05 22:11:34 +0200

    refactored params

--- src/Set-JsonVariables.ps1
+++ src/Set-JsonVariables.ps1
@@ -8,6 +8,7 @@ param (
 )
 $here = Split-Path $MyInvocation.MyCommand.Definition
 $modulePath = Join-Path -Path $here -ChildPath 'JsonVariables.psm1'
+gci $here
 Import-Module $modulePath -Force
 
 Set-JsonVariables -scope $scope -configFile $configFile
